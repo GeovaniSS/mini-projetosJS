ef6c4371ba618233adcc8813a50bd3e0e1f2e6f3
GeovaniSS <geosilvasousa@gmail.com>
2022-01-24 12:45:47 -0300

    Alterações na Calculadora de IMC

--- Calculo-IMC/script.js
+++ Calculo-IMC/script.js
@@ -1,32 +1,50 @@
 var verificarIMC = document.getElementById('button') //Botão
+var limparIMC = document.getElementById('button-limpar') //Botão Limpar
 
 function mostrarIMC() {
-    var peso = document.getElementById('peso').value
-    var altura = document.getElementById('altura').value
-    var IMC = peso/altura**2
+    var peso = document.getElementById('peso').value //Input do peso
+    var altura = document.getElementById('altura').value //Input da altura
+    var IMC = peso/altura**2 //Calculo do IMC
     var resultadoIMC = document.getElementById('resultadoIMC')
-    var textoIMC = document.getElementById('IMC')
+    var valordoIMC = document.getElementById('IMC')
 
-    textoIMC.innerHTML = IMC.toFixed(2)
-
-    if (IMC < 18.5) {
-        document.getElementById('table-calc-0').style.backgroundColor='rgb(235, 253, 234)'
-    }
-    else if (IMC <= 24.9) {
-        document.getElementById('table-calc-1').style.backgroundColor='rgb(235, 253, 234)'
-    }
-    else if (IMC <= 29.9) {
-        document.getElementById('table-calc-2').style.backgroundColor='rgb(235, 253, 234)'
-    }
-    else if (IMC <= 39.9) {
-        document.getElementById('table-calc-3').style.backgroundColor='rgb(235, 253, 234)'
+    if (peso == '' || altura == '') {
+        window.alert('[ERRO] Informe o (Peso) e a (Altura)')
     }
     else {
-        document.getElementById('table-calc-4').style.backgroundColor='rgb(235, 253, 234)'
+        if (IMC < 18.5) {
+            document.getElementById('table-calc-0').style.backgroundColor='rgb(235, 253, 234)'
+        }
+        else if (IMC <= 24.9) {
+            document.getElementById('table-calc-1').style.backgroundColor='rgb(235, 253, 234)'
+        }
+        else if (IMC <= 29.9) {
+            document.getElementById('table-calc-2').style.backgroundColor='rgb(235, 253, 234)'
+        }
+        else if (IMC <= 39.9) {
+            document.getElementById('table-calc-3').style.backgroundColor='rgb(235, 253, 234)'
+        }
+        else {
+            document.getElementById('table-calc-4').style.backgroundColor='rgb(235, 253, 234)'
+        }
+
+        resultadoIMC.style.display = 'flex' //Aparecer o resultado do IMC com o valor do IMC
+        valordoIMC.innerHTML = IMC.toFixed(2)
     }
+}
 
-    resultadoIMC.style.display = 'flex' //Aparecer o resultado do IMC com o valor do IMC
+function Limpar() {
+        peso.value = '' //Limpar o input peso
+        altura.value = '' //Limpar o input altura
 
+        document.getElementById('table-calc-0').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
+        document.getElementById('table-calc-1').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
+        document.getElementById('table-calc-2').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
+        document.getElementById('table-calc-3').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
+        document.getElementById('table-calc-4').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
+
+        resultadoIMC.style.display = 'none' //Resultado invisível 
 }
 
 verificarIMC.addEventListener('click', mostrarIMC)
+limparIMC.addEventListener('click', Limpar)
--- Horario-Sistema/script.js
+++ Horario-Sistema/script.js
@@ -2,27 +2,30 @@ var recarregarPagina = document.getElementsByTagName('body')
 
 function Carregar() {
     var informarHora = document.getElementById('horas')
+    var foto = document.createElement('img')
     var fotoHora = document.getElementById('foto')
     var mensagemdoDia = document.getElementById('msg')
     var hora = new Date().getHours()
     var minuto = new Date().getMinutes()
-    informarHora.innerHTML = `Agora são ${hora} horas e ${minuto}minutos`
-
+    
+    informarHora.innerHTML = `Agora são ${hora} horas e ${minuto} minutos`
+ 
     if (hora < 12) {
         document.body.style.backgroundColor="#e2cd9f"
         mensagemdoDia.innerHTML = 'Bom dia!'
-        fotoHora.innerHTML = '<img src="img/manha.jpg">'
-    
+        foto.src = 'img/manha.jpg'
+        
     } else if (hora <= 18) {
         document.body.style.backgroundColor="#b9846f"
         mensagemdoDia.innerHTML = 'Boa tarde!'
-        fotoHora.innerHTML = '<img src="img/tarde.jpg">'
+        foto.src = 'img/tarde.jpg'
     
     } else {
         document.body.style.backgroundColor="#515154"
         mensagemdoDia.innerHTML = 'Boa noite!'
-        fotoHora.innerHTML = '<img src="img/noite.jpg">'
+        foto.src = 'img/noite.jpg'
     }
+    fotoHora.appendChild(foto)
 }
 
 recarregarPagina.addEventListener('load', Carregar())
