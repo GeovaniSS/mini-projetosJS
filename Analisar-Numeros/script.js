316fffaacbe55bb4db7c776d6c23c6f8c48781a1
GeovaniSS <geosilvasousa@gmail.com>
2022-02-07 22:46:06 -0300

    [fix]: add functions in script

--- Analisar-Numeros/script.js
+++ Analisar-Numeros/script.js
@@ -1,28 +1,29 @@
-const adicionar = document.querySelector('input#adicionar')
-const finalizar = document.querySelector('input#finalizar')
-let array = [] // Variável Global
+const adicionar = document.querySelector('input#adicionar') //Botão adicionar
+const finalizar = document.querySelector('input#finalizar') //Botão finalizar
+const resultado = document.querySelector('div#resultado') //Div do resultado
+let array = [] // O array foi criado no escopo global, pois, caso fosse criado em uma função, a cada chamada o array seria inicializado (limpando os valores)
 
 function adicionarValores() {
     resultado.innerHTML = ''
-    let valor = document.querySelector('input#numero').value
+    let valor = document.querySelector('input#numero').value //Input do número
 
     if (valor === '') {
         window.alert('Valor inválido ou já encontrado na lista')
     }
     else {
-        v = Number(valor)
+        let valorDigitado = Number(valor)
 
-        if (v >= 1 && v <= 100) {
-            let númeroRepetido = vetordeValores(v)
-            if (númeroRepetido == true) {
+        if (valorDigitado >= 1 && valorDigitado <= 100) {
+            let valorRepetido = vetordeValores(valorDigitado)
+            if (valorRepetido == true) {
                 window.alert('Valor inválido ou já encontrado na lista')
             }
             else {
-                listadeValores(v)
+                listadeValores(valorDigitado)
             }
         }
         else {
-            window.alert('[ERRO] Digite um número entre 0 e 100')
+            window.alert('[ERRO] Digite um número entre 1 e 100')
         }    
     } 
 }
@@ -44,35 +45,41 @@ function listadeValores (valor) {
     caixadeValores.appendChild(option)
 }
 
-
 function mostrarResultado() {
     if (array.length == 0) {
         window.alert('[ERRO] Adicione valores antes de finalizar')
     }
     else {
-        const resultado = document.querySelector('div#resultado')
-
-        //Tamanho do Array
-        resultado.innerHTML = `Ao todo temos ${array.length} números cadastrados </br>`
-
-        //Maior valor 
-        array.sort()
-        resultado.innerHTML += `O maior valor informado foi ${array[array.length -1]}  </br>`
-
-        //Menor valor
-        resultado.innerHTML += `O menor valor informado foi ${array[0]}  </br>`
-
-        //Soma dos valores
-        let somaValores = 0
-        for (let i = 0; i < array.length; i++) {
-            somaValores = somaValores + array[i]
-        }
-        resultado.innerHTML += `Somando todos os valores, temos ${somaValores}  </br>`
-
-        //Média dos valores
-        let médiaValores = somaValores / array.length
-        resultado.innerHTML += `A média dos valores digitados é ${médiaValores}  </br>`
+        tamanhoArray()
+        maioreMenor()
+        somaeMédia()
     }
+}
+
+function tamanhoArray() {
+    resultado.innerHTML = `Ao todo temos ${array.length} números cadastrados </br>`
+}
+
+function maioreMenor() {
+    array.sort()
+    //Maior valor 
+    resultado.innerHTML += `O maior valor informado foi ${array[array.length -1]}  </br>`
+
+    //Menor valor
+    resultado.innerHTML += `O menor valor informado foi ${array[0]}  </br>`
+}
+
+function somaeMédia() {
+    //Soma dos valores
+    let somaValores = 0
+    for (let i = 0; i < array.length; i++) {
+        somaValores = somaValores + array[i]
+    }
+    resultado.innerHTML += `Somando todos os valores, temos ${somaValores}  </br>`
+
+    //Média dos valores
+    let médiaValores = somaValores / array.length
+    resultado.innerHTML += `A média dos valores digitados é ${médiaValores}  </br>`
 }
 
 adicionar.addEventListener('click', adicionarValores)
