13c840a17adb5f22a6d1b9e4c28285fa24895273
GeovaniSS <geosilvasousa@gmail.com>
2022-02-10 20:36:04 -0300

    [feat]: IMC script and validations

--- Calculo-IMC/script.js
+++ Calculo-IMC/script.js
@@ -1,50 +1,89 @@
-const verificar = document.getElementById('button') //Botão
-const limpar = document.getElementById('button-limpar') //Botão Limpar
+function myScope() {
+    const check = document.querySelector('#button-verificar') //Botão Verificar
+    const reset = document.querySelector('#button-limpar') //Botão Limpar
+    const tableImc = document.querySelectorAll('table tbody tr') //Linhas da Tabela
+    const weight = document.querySelector('#peso') //Input peso
+    const height = document.querySelector('#altura') //Input altura
+    const result = document.querySelector('div#resultadoIMC') //Div Resultado
+    const imcResult = document.createElement('strong') // Resultado do IMC
 
-function verificarIMC() {
-    const peso = document.getElementById('peso').value //Input do peso
-    const altura = document.getElementById('altura').value //Input da altura
-    let IMC = peso/altura**2 //Calculo do IMC
-    const resultadoIMC = document.getElementById('resultadoIMC')
-    const valordoIMC = document.getElementById('IMC')
+    check.addEventListener('click', checkIMC)
+    reset.addEventListener('click', resetIMC)
 
-    if (peso === '' || altura === '') {
-        window.alert('[ERRO] Informe o (Peso) e a (Altura)')
-    }
-    else {
-        if (IMC < 18.5) {
-            document.getElementById('table-calc-0').style.backgroundColor='rgb(235, 253, 234)'
-        }
-        else if (IMC <= 24.9) {
-            document.getElementById('table-calc-1').style.backgroundColor='rgb(235, 253, 234)'
-        }
-        else if (IMC <= 29.9) {
-            document.getElementById('table-calc-2').style.backgroundColor='rgb(235, 253, 234)'
-        }
-        else if (IMC <= 39.9) {
-            document.getElementById('table-calc-3').style.backgroundColor='rgb(235, 253, 234)'
+    /* Função que valida se o IMC pode ser calculado ou não 
+        - Se os inputs peso e altura forem vazios, o IMC não é calculável
+        - Se os inputs peso e altura não forem vazios, o IMC é calculável
+    */
+    function isCalculate (weight, height) {
+        if (weight.value !== '' || height.value !== '') {
+            return true
         }
         else {
-            document.getElementById('table-calc-4').style.backgroundColor='rgb(235, 253, 234)'
+            return false
         }
+    }
 
-        resultadoIMC.style.display = 'flex' //Aparecer o resultado do IMC com o valor do IMC
-        valordoIMC.innerHTML = IMC.toFixed(2)
+    /* Função que valida se o IMC já foi calculado
+        - Se a table tr possui uma classe active o IMC já foi calculado e a função
+        retorna true
+    */
+    function tableShowImc (table) { 
+        for (let i = 1; i < table.length; i++) {
+            if (table[i].classList.contains('active')) {
+                return true
+            }
+        }
+    }
+
+    //Função que Calcula o IMC
+    function checkIMC() {
+        if (!isCalculate(weight, height)) {
+            window.alert('[ERRO] Digite o peso e a altura para saber seu IMC')
+        }
+        else if (tableShowImc(tableImc)) {
+            window.alert('[ERRO] IMC já encontrado, clique no botão limpar')
+        }
+        else {
+            const IMC = Number(weight.value) / Number(height.value) ** 2
+            
+            result.style.display = 'flex'
+            imcResult.innerHTML = `${IMC.toFixed(2)}`
+            result.appendChild(imcResult)
+
+            if (IMC < 18.5) {
+                tableImc[1].classList.toggle('active')
+            }
+            else if (IMC < 24.9) {
+                tableImc[2].classList.toggle('active')
+            }
+            else if (IMC < 30) {
+                tableImc[3].classList.toggle('active')
+            }
+            else if (IMC < 40) {
+                tableImc[4].classList.toggle('active')
+            }
+            else {
+                tableImc[5].classList.toggle('active')
+            }            
+        }
+    }
+
+    //Função que Limpa o IMC
+    function resetIMC() {
+        if (weight.value == '' || height.value == '') {
+            window.alert('[ERRO] Digite o peso e a altura antes de limpar')
+        }
+        else {
+            result.style.display = 'none'
+            result.removeChild(imcResult)
+            weight.value = ''
+            height.value = ''
+            weight.focus()
+    
+            for (let i = 1; i < tableImc.length; i++) {
+                tableImc[i].classList.remove('active')
+            }
+        }
     }
 }
-
-function limparIMC() {
-        peso.value = '' //Limpar o input peso
-        altura.value = '' //Limpar o input altura
-
-        document.getElementById('table-calc-0').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
-        document.getElementById('table-calc-1').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
-        document.getElementById('table-calc-2').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
-        document.getElementById('table-calc-3').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
-        document.getElementById('table-calc-4').style.backgroundColor='rgb(255, 255, 255)' //Cor padrão
-
-        resultadoIMC.style.display = 'none' //Resultado invisível 
-}
-
-verificar.addEventListener('click', verificarIMC)
-limpar.addEventListener('click', limparIMC)
+myScope()
