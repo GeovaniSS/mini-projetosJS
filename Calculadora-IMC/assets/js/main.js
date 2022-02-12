8519542d0706a087e84e7e51f1bd693bd2f4009f
GeovaniSS <geosilvasousa@gmail.com>
2022-02-12 18:23:54 -0300

    [fix]: imc script

--- Calculadora-IMC/assets/js/main.js
+++ Calculadora-IMC/assets/js/main.js
@@ -1,20 +1,70 @@
 function myScope() {
-    const weightText = document.querySelector('div#weightText') // Text Weight
-    const heightText = document.querySelector('div#heightText') // Text Height
-    const messageWeight = document.createElement('p') // Peso Inválido
-    const messageHeight = document.createElement('p') // Altura Inválida
+    const check = document.querySelector('input#enviar') // Botão Enviar
+    const clean = document.querySelector('input#limpar') // Botão Limpar
     const weight = document.querySelector('input#weight') //Input Weight
     const height = document.querySelector('input#height') //Input Height
     const tableIMC = document.querySelectorAll('table tbody tr') // Tabela 
-    const enviar = document.querySelector('input#enviar') // Botão Enviar
-    const limpar = document.querySelector('input#limpar') // Botão Limpar
     const result = document.querySelector('div#result') // Div Resultado
-    const strong = document.createElement('strong') // Resultado IMC
+    const imcResult = document.createElement('strong') // Resultado IMC
 
-    enviar.addEventListener('click', checkIMC)
-    limpar.addEventListener('click', cleanIMC)
-
+    check.addEventListener('click', checkIMC)
+    clean.addEventListener('click', cleanIMC)    
+    
     function checkIMC() {
+        const weightText = document.querySelector('div#weightText') // Text Weight
+        const heightText = document.querySelector('div#heightText') // Text Height
+        const messageWeight = createMessage('Peso Inválido') // Peso Inválido
+        const messageHeight = createMessage('Altura Inválida') //Altura Inválida
+
+        if (tableShowImc(tableIMC)) {
+            window.alert('[ERRO] Limpe os valores para saber novamente')
+        }
+        
+        if (invalidWeight(weight)) {
+            weight.style.borderColor = 'red'
+            weightText.appendChild(messageWeight)
+        }
+
+        if (invalidHeight(height)) {
+            height.style.borderColor = 'red'
+            heightText.appendChild(messageHeight)
+        }
+
+        else {
+            const IMC = Number(weight.value) / Number(height.value) ** 2
+            const classIMC = ['Magreza', 'Normal', 'Sobrepeso', 'Obesidade', 'Obesidade Grave']
+            
+            if (IMC < 18.5) {
+                tableIMC[1].classList.add('active')
+                showIMC(classIMC[0], IMC)
+            }
+            else if (IMC < 24.9) {
+                tableIMC[2].classList.add('active')
+                showIMC(classIMC[1], IMC)
+            }
+            else if (IMC < 29.9) {
+                tableIMC[3].classList.add('active')
+                showIMC(classIMC[2], IMC)
+            }
+            else if (IMC < 39,9) {
+                tableIMC[4].classList.add('active')
+                showIMC(classIMC[3], IMC)
+            }
+            else {
+                tableIMC[5].classList.add('active')
+                showIMC(classIMC[4], IMC)
+            }
+        }
+
+        /*Função que valida se o resultado já foi mostrado na tabela*/
+        function tableShowImc(table) {
+            for (let i = 1; i < table.length; i++) {
+                if (table[i].classList.contains('active')) {
+                    return true
+                }
+            }
+        }
+        
         /* 2kg > weight < 595kg*/
         function invalidWeight(weight) {
             if (weight.value === '' || Number(weight.value) < 2 || Number(weight.value) > 595) {
@@ -35,55 +85,27 @@ function myScope() {
             }
         }
         
-        if (invalidWeight(weight)) {
-            weight.style.borderColor = 'red'
-            messageWeight.innerHTML = 'Peso inválido'
-            weightText.appendChild(messageWeight)
+        /*Função que valida se o resultado já foi mostrado na tabela*/
+        function createMessage(msg) {
+            const message = document.createElement('p')
+            message.innerHTML = msg
+            return message
         }
-
-        if (invalidHeight(height)) {
-            height.style.borderColor = 'red'
-            messageHeight.innerHTML = 'Altura Inválida'
-            heightText.appendChild(messageHeight)
+        
+        /*Função que mostra o resultado do IMC*/
+        function showIMC(classIMC, IMC) {
+            result.style.display = 'flex'
+            imcResult.innerHTML = `${IMC.toFixed(2)} (${classIMC})`
+            result.appendChild(imcResult)
         }
-
-        else {
-            const IMC = Number(weight.value) / Number(height.value) ** 2
-            const classIMC = ['Magreza', 'Normal', 'Sobrepeso', 'Obesidade', 'Obesidade Grave']
-            
-            if (IMC < 18.5) {
-                tableIMC[1].classList.add('active')
-                imcResult(classIMC[0], IMC)
-            }
-            else if (IMC < 24.9) {
-                tableIMC[2].classList.add('active')
-                imcResult(classIMC[1], IMC)
-            }
-            else if (IMC < 29.9) {
-                tableIMC[3].classList.add('active')
-                imcResult(classIMC[2], IMC)
-            }
-            else if (IMC < 39,9) {
-                tableIMC[4].classList.add('active')
-                imcResult(classIMC[3], IMC)
-            }
-            else {
-                tableIMC[5].classList.add('active')
-                imcResult(classIMC[4], IMC)
-            }
-
-            function imcResult(classIMC, IMC) {
-                result.style.display = 'flex'
-                strong.innerHTML = `${IMC.toFixed(2)} (${classIMC})`
-                result.appendChild(strong)
-            }
-        }
-
+        
+        /*Função que limpa a validação do input Peso*/
         weight.addEventListener('focus', () => {
             weight.style.borderColor = 'initial'
             weightText.removeChild(messageWeight)
         })
 
+        /*Função que limpa a validação do input Altura*/
         height.addEventListener('focus', () => {
             height.style.borderColor = 'initial'
             heightText.removeChild(messageHeight)
@@ -103,7 +125,7 @@ function myScope() {
             }   
 
             result.style.display = 'none'
-            result.removeChild('strong')
+            result.removeChild(imcResult)
         }
     }
 }
