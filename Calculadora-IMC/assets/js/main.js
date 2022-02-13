68e6e9e4ff0747d43e24503122c56e9cfeb2f3b0
GeovaniSS <geosilvasousa@gmail.com>
2022-02-13 00:01:54 -0300

    [feat]: weight e height validation

--- Calculadora-IMC/assets/js/main.js
+++ Calculadora-IMC/assets/js/main.js
@@ -1,132 +1,131 @@
-function myScope() {
-    const check = document.querySelector('input#enviar') // Botão Enviar
-    const clean = document.querySelector('input#limpar') // Botão Limpar
-    const weight = document.querySelector('input#weight') //Input Weight
-    const height = document.querySelector('input#height') //Input Height
-    const tableIMC = document.querySelectorAll('table tbody tr') // Tabela 
-    const result = document.querySelector('div#result') // Div Resultado
-    const imcResult = document.createElement('strong') // Resultado IMC
+/*Capturando o evento de submit do form */
+const form = document.querySelector('.form')
 
-    check.addEventListener('click', checkIMC)
-    clean.addEventListener('click', cleanIMC)    
-    
-    function checkIMC() {
-        const weightText = document.querySelector('div#weightText') // Text Weight
-        const heightText = document.querySelector('div#heightText') // Text Height
-        const messageWeight = createMessage('Peso Inválido') // Peso Inválido
-        const messageHeight = createMessage('Altura Inválida') //Altura Inválida
+form.addEventListener('submit', (e) => {
+    e.preventDefault() //Tira a configuração padrão do evento de submit
 
-        if (tableShowImc(tableIMC)) {
-            window.alert('[ERRO] Limpe os valores para saber novamente')
-        }
-        
-        if (invalidWeight(weight)) {
-            weight.style.borderColor = 'red'
-            weightText.appendChild(messageWeight)
-        }
+    //Caixas que recebem a mensagem de peso/altura inválida
+    const invalidWeightMessage = e.target.querySelector('.weightText') 
+    const invalidHeightMessage = e.target.querySelector('.heightText') 
 
-        if (invalidHeight(height)) {
-            height.style.borderColor = 'red'
-            heightText.appendChild(messageHeight)
-        }
+    //Inputs do Peso e da Altura
+    const inputWeight = e.target.querySelector('#weight')
+    const inputHeight = e.target.querySelector('#height')
 
-        else {
-            const IMC = Number(weight.value) / Number(height.value) ** 2
-            const classIMC = ['Magreza', 'Normal', 'Sobrepeso', 'Obesidade', 'Obesidade Grave']
-            
-            if (IMC < 18.5) {
-                tableIMC[1].classList.add('active')
-                showIMC(classIMC[0], IMC)
-            }
-            else if (IMC < 24.9) {
-                tableIMC[2].classList.add('active')
-                showIMC(classIMC[1], IMC)
-            }
-            else if (IMC < 29.9) {
-                tableIMC[3].classList.add('active')
-                showIMC(classIMC[2], IMC)
-            }
-            else if (IMC < 39,9) {
-                tableIMC[4].classList.add('active')
-                showIMC(classIMC[3], IMC)
-            }
-            else {
-                tableIMC[5].classList.add('active')
-                showIMC(classIMC[4], IMC)
-            }
-        }
+    //O valor dos inputs Peso e Altura convertidos para Number
+    const weight = Number(inputWeight.value)
+    const height = Number(inputHeight.value)
 
-        /*Função que valida se o resultado já foi mostrado na tabela*/
-        function tableShowImc(table) {
-            for (let i = 1; i < table.length; i++) {
-                if (table[i].classList.contains('active')) {
-                    return true
-                }
-            }
-        }
-        
-        /* 2kg > weight < 595kg*/
-        function invalidWeight(weight) {
-            if (weight.value === '' || Number(weight.value) < 2 || Number(weight.value) > 595) {
-                return true
-            }
-            else {
-                return false
-            }
-        }
-
-         /* 0.63 > height < 2.51m*/
-        function invalidHeight(height) {
-            if (height.value === '' || Number(height.value) < 0.63 || Number(height.value) > 2.51) {
-                return true
-            }
-            else {
-                return false
-            }
-        }
-        
-        /*Função que valida se o resultado já foi mostrado na tabela*/
-        function createMessage(msg) {
-            const message = document.createElement('p')
-            message.innerHTML = msg
-            return message
-        }
-        
-        /*Função que mostra o resultado do IMC*/
-        function showIMC(classIMC, IMC) {
-            result.style.display = 'flex'
-            imcResult.innerHTML = `${IMC.toFixed(2)} (${classIMC})`
-            result.appendChild(imcResult)
-        }
-        
-        /*Função que limpa a validação do input Peso*/
-        weight.addEventListener('focus', () => {
-            weight.style.borderColor = 'initial'
-            weightText.removeChild(messageWeight)
-        })
-
-        /*Função que limpa a validação do input Altura*/
-        height.addEventListener('focus', () => {
-            height.style.borderColor = 'initial'
-            heightText.removeChild(messageHeight)
-        })
+    //Condição para o input do Peso ser inválido
+    if (!weight || invalidWeight(weight)) {
+        invalidMessage('Peso inválido', inputWeight, invalidWeightMessage)
     }
 
-    function cleanIMC() {
-        if (weight.value === '' || height.value === '') {
-            window.alert('Digite valores antes de limpar')
-        }
-        else {
-            weight.value = ''
-            height.value = ''
-            
-            for (let i = 1; i < tableIMC.length; i++) {
-                tableIMC[i].classList.remove('active')
-            }   
+    //Condição para o input da Altura ser inválido
+    if (!height || invalidHeight(height)) {
+        invalidMessage('Altura inválida', inputHeight, invalidHeightMessage)
+    }
 
-            result.style.display = 'none'
-            result.removeChild(imcResult)
-        }
+    //Condição para os inputs do Peso e Altura serem válidos
+    if (weight && height && !invalidWeight(weight) && !invalidHeight(height)) {
+        const imc = getImc(weight, height)
+        const imcStatus = getImcStatus(imc)
+        const message = `Seu imc é: ${imc} (${imcStatus})`
+        setImcResult(message)  
+    }
+    
+    //Evento que remove a borda vermelha e a mensagem inválida do input Peso
+    inputWeight.addEventListener('focus', () =>  {
+        inputWeight.classList.remove('invalidMessage')
+        invalidWeightMessage.innerHTML = ''
+    })
+
+    //Evento que remove a borda vermelha e a mensagem inválida do input Altura
+    inputHeight.addEventListener('focus', () => {
+        inputHeight.classList.remove('invalidMessage')
+        invalidHeightMessage.innerHTML = ''
+    })
+})
+
+// Função que retorna true se o peso for menor que 2 ou maior que 560
+function invalidWeight(weight) {
+    if (weight < 2 || weight > 560) {
+        return true
+    }
+    else {
+        return false
     }
 }
-myScope()
+
+// Função que retorna true se a altura for menor que 0.63 ou maior que 2.63
+function invalidHeight(height) {
+    if (height < 0.63 || height > 2.63) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+/* Função que é executada quando o peso ou a altura são inválidos. Ela cria uma mensagem inválida e muda a cor da borda do input*/
+function invalidMessage(msg, input, invalidText) {
+    input.classList.add('invalidMessage') //A borda do input fica vermelha
+
+    invalidText.innerHTML = ''
+    const message = createMessage(msg)
+    invalidText.appendChild(message)
+}
+
+// Funçaõ que cria um parágrafo, adiciona um conteúdo e retorna para a chamada
+function createMessage(msg) {
+    const message = document.createElement('p')
+    message.innerHTML = msg
+    return message
+}
+
+// Função que calcula o IMC 
+function getImc(weight, height) {
+    const imc = weight / height ** 2
+    return imc.toFixed(2)
+}
+
+/* Função que retorna o status do IMC e altera a cor da linha da tabela de acordo com o valor do IMC*/
+function getImcStatus (imc) {
+    const status = ['Magreza', 'Normal', 'Sobrepeso', 'Obesidade', 'Obesidade Grave']
+    const imcTable = document.querySelectorAll('table tbody tr')
+
+    for (let i = 1; i < imcTable.length; i++) {
+        imcTable[i].classList.remove('active')
+    }
+
+    if (imc < 18.5) {
+        imcTable[1].classList.add('active')
+        return status[0]
+    }
+    if (imc <= 24.9) {
+        imcTable[2].classList.add('active')
+        return status[1]
+    }
+    if (imc <= 29.9) {
+        imcTable[3].classList.add('active')
+        return status[2]
+    } 
+    if (imc <= 39.09) {
+        imcTable[4].classList.add('active')
+        return status[3]
+    } 
+    if (imc > 40) {
+        imcTable[5].classList.add('active')
+        return status[4]
+    }
+}
+
+/* Função que mostra o resultado do IMC*/
+function setImcResult (msg) {
+    const imcResult = document.querySelector('#result')
+    imcResult.innerHTML = ''
+    imcResult.style.display = 'flex'
+
+    const message = createMessage(msg)
+    imcResult.appendChild(message)
+}
