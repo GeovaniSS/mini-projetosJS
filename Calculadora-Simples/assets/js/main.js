6b7742e7a428836832482e9c783453985f43de51
GeovaniSS <geosilvasousa@gmail.com>
2022-03-16 11:10:43 -0300

    [fix]: clear display fixed

--- Calculadora-Simples/assets/js/main.js
+++ Calculadora-Simples/assets/js/main.js
@@ -1,10 +1,3 @@
-// Utilizar uma função fábrica (1 apenas) para criar o objeto da calculadora. A Calculadora inteira vai ser um objeto
-// Utilizar try, cat e throw para tratar erros
-// Quando não conseguir realizar a conta, um alerta será dado para o usuário com "Conta Inválida"
-// Criar todas as funcionalidades dentro da função fábrica (factory)
-// Revisar o método slice de strings
-// Estudar um pouco sobre o método eval()
-// Revisar as explicações do Luís Otávio
 const createCalculator = () => {
   const display = document.querySelector('#display')
   const buttons = document.querySelector('.buttons')
@@ -18,11 +11,11 @@ const createCalculator = () => {
     handleNumberButtonClick() {
       buttons.addEventListener('click', (e) => {
         const el = e.target
-  
-        if (el.classList.contains('btn-number')) this.updateDisplay(el)
-        if(el.classList.contains('btn-clear')) this.clearDisplay()
+
+        if(el.classList.contains('btn-number')) this.updateDisplay(el)
+        if(el.classList.contains('btn-clear'))  this.clearDisplay()
         if(el.classList.contains('btn-delete')) this.removeDisplay()
-        if(el.classList.contains('btn-equal')) this.resultDisplay()
+        if(el.classList.contains('btn-equal'))  this.resultDisplay()
       })
     },
 
@@ -34,12 +27,12 @@ const createCalculator = () => {
       })
     },
 
-    clearDisplay() {
-      display.value = ''
-    },
-
     updateDisplay(el) {
       display.value += el.innerText 
+    },
+    
+    clearDisplay() {
+      display.value = ''
     },
 
     removeDisplay() {
@@ -55,7 +48,6 @@ const createCalculator = () => {
         display.value = result
       } catch(error) {
         alert('Conta Inválida')
-        display.value = ''
       }
     }
   }
